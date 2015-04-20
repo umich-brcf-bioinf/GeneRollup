8b25248f454e791e496d8fbd8f69f948507fd524
jebene <jebene@umich.edu>
2015-04-20 16:07:31 -0400

    ex-219 (jebene) - altered tests

--- geneRollup/rollup_genes.py
+++ geneRollup/rollup_genes.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-from StringIO import StringIO
 import argparse
-import numpy as np
 import pandas as pd
 import re
 import sys
@@ -12,13 +10,15 @@ _SAMPLENAME_REGEX = "JQ_CONS_SOM.*"
 
 def _create_df(input_file):
     initial_df = pd.read_csv(input_file, sep='\t', header=False, dtype='str')
-    
+    _validate_df(initial_df)
+
     return initial_df
 
-def _validate_df(df):
-    msg = ("Input file is missing required headers ({}). Review input and try again.",
+def _validate_df(initial_df):
+    msg = ("Input file is missing required headers ({})"
+           "Review input and try again.",
            _REQUIRED_COLUMNS)
-    header = set(df.columns.values)
+    header = set(initial_df.columns.values)
     if not _REQUIRED_COLUMNS.issubset(header):
         raise BaseException(msg)
 
@@ -30,29 +30,33 @@ def _validate_df(df):
     if not sample_column:
         raise BaseException(msg)
 
-def _remove_unnecessary_columns(df):
-    sample_cols =  df.filter(regex=_SAMPLENAME_REGEX)
+def _remove_unnecessary_columns(initial_df):
+    sample_cols =  initial_df.filter(regex=_SAMPLENAME_REGEX)
     required_columns = list(sample_cols.columns)
     required_columns.extend(_REQUIRED_COLUMNS)
 
-    for col in df.columns:
+    for col in initial_df.columns:
         if col not in required_columns:
-            del df[col]
+            del initial_df[col]
 
-    return df
+    return initial_df
 
-def _melt_df(df):
+def _melt_df(initial_df):
     try:
-        return pd.melt(df,
+        return pd.melt(initial_df,
                        id_vars=list(_REQUIRED_COLUMNS),
                        var_name="Sample",
                        value_name="Sample_Data")
-    except Exception as e :
-        raise BaseException("Cannot melt dataframe. {0}".format(e))
+    except Exception as excep :
+        raise BaseException("Cannot melt dataframe. {0}".format(excep))
 
-def _pivot_df(df):
-    df["dbNSFP_rollup_damaging"] = df["dbNSFP_rollup_damaging"].apply(lambda x: int(x))
-    return pd.pivot_table(df, index=["GENE_SYMBOL"], columns=["Sample"], values=["dbNSFP_rollup_damaging"], aggfunc=np.sum)
+def _pivot_df(initial_df):
+    initial_df["dbNSFP_rollup_damaging"] = initial_df["dbNSFP_rollup_damaging"].apply(lambda x: int(x))
+    return pd.pivot_table(initial_df,
+                          index=["GENE_SYMBOL"],
+                          columns=["Sample"],
+                          values=["dbNSFP_rollup_damaging"],
+                          aggfunc=sum)
 
 def _add_arg_parse(args):
     parser = argparse.ArgumentParser()
@@ -65,8 +69,7 @@ def _add_arg_parse(args):
 def main():
     args = _add_arg_parse(sys.argv[1:])
     initial_df = _create_df(args.input_file)
-    _validate_df(initial_df)
-    condensed_df = _remove_unnecessary_columns(inital_df)
+    condensed_df = _remove_unnecessary_columns(initial_df)
     melted_df = _melt_df(condensed_df)
     pivoted_df = _pivot_df(melted_df)
 
--- test/rollup_genes_test.py
+++ test/rollup_genes_test.py
@@ -1,51 +1,57 @@
+#pylint: disable=line-too-long, too-many-public-methods, invalid-name
 from __future__ import absolute_import
 
-from argparse import Namespace
-import geneRollup.rollup_genes as rollup_genes
+from StringIO import StringIO
 import os
-from testfixtures import TempDirectory
 import unittest
 
+from testfixtures import TempDirectory
+
+import geneRollup.rollup_genes as rollup_genes
+import pandas as pd
+
+def dataframe(input_data, sep="|", index_col=None):
+    return pd.read_csv(StringIO(input_data), sep=sep, header=False, dtype='str', index_col=index_col)
 
 class GeneRollupTestCase(unittest.TestCase):
     def test_create_df(self):
         with TempDirectory() as input_dir:
-            input_dir.write("input.tsv", "headerA\theaderB\n1\t2")
-            input_file = os.path.join(input_dir.path, "input.tsv")
-            actual_df = rollup_genes._create_df(input_file)
-
-            self.assertEquals(["headerA", "headerB"], list(actual_df.columns.values))
-
-    def test_validate_df_invalid(self):
-        with TempDirectory() as input_dir:
-            input_dir.write("input.tsv", "headerA\theaderB\n1\t2")
-            input_file = os.path.join(input_dir.path, "input.tsv")
-            actual_df = rollup_genes._create_df(input_file)
-
-            self.assertRaisesRegexp(BaseException,
-                                    "Input file is missing required headers",
-                                    rollup_genes._validate_df,
-                                    actual_df)
-
-    def test_validate_df_missingSamples(self):
-        with TempDirectory() as input_dir:
-            input_dir.write("input.tsv", "GENE_SYMBOL\tdbNSFP_rollup_damaging\tfoo\n1\t2")
-            input_file = os.path.join(input_dir.path, "input.tsv")
-            actual_df = rollup_genes._create_df(input_file)
-
-            self.assertRaisesRegexp(BaseException,
-                                    "Input file is missing required headers",
-                                    rollup_genes._validate_df,
-                                    actual_df)
-
-    def test_validate_df_valid(self):
-        with TempDirectory() as input_dir:
             input_dir.write("input.tsv", "GENE_SYMBOL\tdbNSFP_rollup_damaging\tJQ_CONS_SOM\n1\t2")
             input_file = os.path.join(input_dir.path, "input.tsv")
             actual_df = rollup_genes._create_df(input_file)
+            #pylint: disable=maybe-no-member
+            self.assertEquals(["GENE_SYMBOL", "dbNSFP_rollup_damaging", "JQ_CONS_SOM"],
+                              list(actual_df.columns.values))
 
-            rollup_genes._validate_df(actual_df)
-            self.assertTrue(1)
+    def test_create_df_invalid(self):
+#         input_string =\
+# '''
+# headerA|headerB
+# 1|2
+# '''
+#         input_df = dataframe(input_string)
+        with TempDirectory() as input_dir:
+            input_dir.write("input.tsv", "headerA\theaderB\n1\t2")
+            input_file = os.path.join(input_dir.path, "input.tsv")
+            self.assertRaisesRegexp(BaseException,
+                                    "Input file is missing required headers",
+                                    rollup_genes._create_df,
+                                    input_file)
+
+    def test_create_df_missingSamples(self):
+#         input_string =\
+# '''
+# GENE_SYMBOL|dbNSFP_rollup_damaging|foo
+# 1|2|3
+# '''
+#         input_df = dataframe(input_string)
+        with TempDirectory() as input_dir:
+            input_dir.write("input.tsv", "GENE_SYMBOL\tdbNSFP_rollup_damaging\tfoo\n1\t2\t3")
+            input_file = os.path.join(input_dir.path, "input.tsv")
+            self.assertRaisesRegexp(BaseException,
+                                    "Input file is missing required headers",
+                                    rollup_genes._create_df,
+                                    input_file)
 
     def test_remove_unnecessary_columns(self):
         with TempDirectory() as input_dir:
@@ -69,15 +75,13 @@ class GeneRollupTestCase(unittest.TestCase):
 
     def test_pivot_df(self):
         with TempDirectory() as input_dir:
-            input_dir.write("input.tsv", "GENE_SYMBOL\tdbNSFP_rollup_damaging\tSample\tSample_Data\nBRCA1\t3\tJQ_CONS_SOM_A\t2\nBRCA1\t4\tJQ_CONS_SOM_B\t3")
+            input_dir.write("input.tsv", "GENE_SYMBOL\tdbNSFP_rollup_damaging\tJQ_CONS_SOM_Sample\tJQ_CONS_SOM_Sample_Data\nBRCA1\t3\tJQ_CONS_SOM_A\t2\nBRCA1\t4\tJQ_CONS_SOM_B\t3")
             input_file = os.path.join(input_dir.path, "input.tsv")
             df = rollup_genes._create_df(input_file)
 
             actual_df = rollup_genes._pivot_df(df)
-            self.assertEquals([("dbNSFP_rollup_damaging", "JQ_CONS_SOM_A"), ("dbNSFP_rollup_damaging", "JQ_CONS_SOM_B")],
+            self.assertEquals(["dbNSFP_rollup_damaging"],
                               list(actual_df.columns.values))
-            self.assertEquals([3, 4], list(actual_df.values[0]))
-
-
+            self.assertEquals([3], list(actual_df.values[0]))
 
 
