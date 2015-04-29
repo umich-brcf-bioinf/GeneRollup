b97ffd5d65392ac6f50d054f436503931ba91ed0
jebene <jebene@umich.edu>
2015-04-29 16:03:14 -0400

    ex-221 (jebene) added snpEff impacts

--- geneRollup/rollup_genes.py
+++ geneRollup/rollup_genes.py
@@ -1,13 +1,195 @@
 #!/usr/bin/env python
 import argparse
+import numpy as np
 import pandas as pd
 import re
 import sys
 
-
-_REQUIRED_COLUMNS = set(["GENE_SYMBOL", "dbNSFP_rollup_damaging"])
+_REQUIRED_COLUMNS = set(["GENE_SYMBOL",
+                         "dbNSFP_rollup_damaging",
+                         "SNPEFF_TOP_EFFECT_IMPACT"])
 _SAMPLENAME_REGEX = "JQ_CONS_SOM.*"
-_DAMAGING_LABEL = "damaging votes"
+
+class dbNSFP(object):
+    #pylint: disable=invalid-name
+    def __init__(self):
+        self.damaging_column = "dbNSFP_rollup_damaging"
+        self.damaging_rank_column = "dbNSFP|overall damaging rank"
+        self.column_label = "damaging votes"
+
+    def melt_df(self, initial_df):
+        try:
+            melted_df = pd.melt(initial_df,
+                               id_vars=list(_REQUIRED_COLUMNS),
+                               var_name="Sample",
+                               value_name="Sample_Data")
+            #pylint: disable=line-too-long, unnecessary-lambda
+            melted_df[self.damaging_column] = melted_df[self.damaging_column].apply(lambda x: str(x))
+            melted_df["Sample_Data"] = melted_df["Sample_Data"].apply(lambda x: str(x))
+
+            melted_df = melted_df[melted_df["GENE_SYMBOL"] != "."]
+
+            return melted_df
+        except Exception as excep :
+            raise BaseException("Cannot melt dataframe. {0}".format(excep))
+
+    def pivot_df(self, initial_df):
+        #pylint: disable=line-too-long, unnecessary-lambda
+        initial_df[self.damaging_column] = initial_df[self.damaging_column].apply(lambda x: int(x))
+        pivoted_df = pd.pivot_table(initial_df,
+                              index=["GENE_SYMBOL"],
+                              columns=["Sample"],
+                              values=[self.damaging_column],
+                              aggfunc=sum)
+        pivoted_df = pivoted_df.applymap(lambda x: None if x == 0 else str(x))
+        pivoted_df.fillna(value="0", inplace=True)
+
+        return pivoted_df
+
+    def calculate_rank(self, initial_df):
+        #pylint: disable=line-too-long, unnecessary-lambda
+        initial_df[self.damaging_rank_column] = initial_df[self.damaging_column].sum(axis=1)
+        initial_df = initial_df.sort(self.damaging_rank_column, ascending=0)
+        initial_df[self.damaging_rank_column] = initial_df[self.damaging_rank_column].rank(ascending=0, method="min")
+
+        try:
+            initial_df[self.damaging_rank_column] = initial_df[self.damaging_rank_column].apply(lambda x: str(int(x)))
+        except ValueError:
+            pass
+
+        return initial_df
+
+    def change_col_order(self, new_column_names):
+        ordered_names = []
+        for col in new_column_names:
+            if col == self.damaging_rank_column:
+                ordered_names.insert(0, col)
+            else:
+                ordered_names.append(col)
+
+        return ordered_names
+
+
+class SnpEff(object):
+    #pylint: disable=invalid-name
+    def __init__(self):
+        self.impact_column = "SNPEFF_TOP_EFFECT_IMPACT"
+        self.impact_rank_column = "SnpEff|overall impact rank"
+        self.tmp_impact_column = "SnpEff_Impact"
+        self.score_column = "SnpEff_impact_score"
+        self.possible_values = ["HIGH", "MODERATE", "LOW", "MODIFIER"]
+        self.column_label = "impact"
+
+    def melt_df(self, initial_df):
+        try:
+            melted_df = pd.melt(initial_df,
+                               id_vars=list(_REQUIRED_COLUMNS),
+                               var_name="Sample",
+                               value_name="Sample_Data")
+            #pylint: disable=line-too-long, unnecessary-lambda
+            melted_df[self.impact_column] = melted_df[self.impact_column].apply(lambda x: str(x))
+            melted_df["Sample_Data"] = melted_df["Sample_Data"].apply(lambda x: str(x))
+
+            melted_df = melted_df[melted_df["GENE_SYMBOL"] != "."]
+
+            return melted_df
+        except Exception as excep :
+            raise BaseException("Cannot melt dataframe. {0}".format(excep))
+
+    def pivot_df(self, initial_df):
+        #pylint: disable=line-too-long, unnecessary-lambda
+        pivoted_df = pd.pivot_table(initial_df,
+                              index=["GENE_SYMBOL", "Sample"],
+                              columns=[self.impact_column],
+                              values=["Sample_Data"],
+                              #pylint: disable=no-member
+                              aggfunc=np.count_nonzero,
+                              fill_value=0)
+        pivoted_df.fillna(value="0", inplace=True)
+        pivoted_df = pivoted_df.applymap(lambda x: int(x))
+
+        pivoted_df = pivoted_df["Sample_Data"]
+
+        return pivoted_df
+
+    def _calculate_impacts(self, initial_df):
+        high = initial_df["HIGH"].apply(lambda x: "h" * x)
+        moderate =  initial_df["MODERATE"].apply(lambda x: "m" * x)
+        low = initial_df["LOW"].apply(lambda x: "l" * x)
+        modifier = initial_df["MODIFIER"].apply(lambda x: "x" * x)
+
+        initial_df[self.tmp_impact_column] = high + moderate + low + modifier
+
+        return initial_df
+
+    def _calculate_score(self, initial_df):
+        high = initial_df["HIGH"] * 100000.0
+        moderate = initial_df["MODERATE"]
+        low = initial_df["LOW"]/100000.0
+        modifier = initial_df["MODIFIER"]/10**12
+
+        initial_df[self.score_column] = high + moderate + low + modifier
+
+        del initial_df["HIGH"]
+        del initial_df["MODERATE"]
+        del initial_df["LOW"]
+        del initial_df["MODIFIER"]
+
+        return initial_df
+
+    def _calculate_impact_score(self, initial_df):
+        for item in self.possible_values:
+            #pylint: disable=line-too-long
+            initial_df[item + "_sum"] = initial_df[item + "_initial_sum"].sum(axis=1)
+
+        initial_df[self.score_column] = initial_df[self.impact_rank_column]
+
+        return initial_df
+
+    def _calculate_impact_rank(self, initial_df):
+        #pylint: disable=line-too-long
+        initial_df = initial_df.sort(self.impact_rank_column, ascending=0)
+        initial_df[self.impact_rank_column] = initial_df[self.impact_rank_column].rank(ascending=0, method="min")
+
+        return initial_df
+
+    def calculate_rank(self, initial_df):
+        #pylint: disable=line-too-long
+        for item in self.possible_values:
+            if item not in initial_df.columns:
+                initial_df[item] = 0
+            initial_df[item + "_initial_sum"] = initial_df[item].map(int)
+
+        impact_df = self._calculate_impacts(initial_df)
+        scored_df = self._calculate_score(impact_df)
+
+        expanded_df = scored_df.unstack()
+
+        expanded_df[self.impact_rank_column] = expanded_df[self.score_column].sum(axis=1)
+        expanded_df = self._calculate_impact_score(expanded_df)
+        expanded_df = self._calculate_impact_rank(expanded_df)
+
+        del expanded_df[self.score_column]
+        for item in self.possible_values:
+            del expanded_df[item + "_initial_sum"]
+
+        try:
+            expanded_df[self.impact_rank_column] = expanded_df[self.impact_rank_column].apply(lambda x: str(int(x)))
+        except ValueError:
+            pass
+
+        return expanded_df
+
+    def change_col_order(self, new_column_names):
+        ordered_names = []
+        for col in new_column_names:
+            if col == self.impact_rank_column:
+                ordered_names.insert(0, col)
+            else:
+                ordered_names.append(col)
+
+        return ordered_names
+
 
 def _create_df(input_file):
     initial_df = pd.read_csv(input_file, sep='\t', header=False, dtype='str')
@@ -42,46 +224,7 @@ def _remove_unnecessary_columns(initial_df):
 
     return initial_df
 
-def _melt_df(initial_df):
-    try:
-        melted_df = pd.melt(initial_df,
-                           id_vars=list(_REQUIRED_COLUMNS),
-                           var_name="Sample",
-                           value_name="Sample_Data")
-        #pylint: disable=line-too-long, unnecessary-lambda
-        melted_df["dbNSFP_rollup_damaging"] = melted_df["dbNSFP_rollup_damaging"].apply(lambda x: str(x))
-        melted_df["Sample_Data"] = melted_df["Sample_Data"].apply(lambda x: str(x))
-
-        melted_df = melted_df[melted_df["GENE_SYMBOL"] != "."]
-
-        return melted_df
-    except Exception as excep :
-        raise BaseException("Cannot melt dataframe. {0}".format(excep))
-
-def _pivot_df(initial_df):
-    #pylint: disable=line-too-long, unnecessary-lambda
-    initial_df["dbNSFP_rollup_damaging"] = initial_df["dbNSFP_rollup_damaging"].apply(lambda x: int(x))
-    pivoted_df = pd.pivot_table(initial_df,
-                          index=["GENE_SYMBOL"],
-                          columns=["Sample"],
-                          values=["dbNSFP_rollup_damaging"],
-                          aggfunc=sum)
-    pivoted_df = pivoted_df.applymap(lambda x: None if x == 0 else str(x))
-    pivoted_df.fillna(value="0", inplace=True)
-
-    return pivoted_df
-
-def _rearrange_columns(initial_df):
-    def _change_col_order(new_column_names):
-        ordered_names = []
-        for col in new_column_names:
-            if col == "dbNSFP|overall damaging rank":
-                ordered_names.insert(0, col)
-            else:
-                ordered_names.append(col)
-
-        return ordered_names
-
+def _rearrange_columns(initial_df, obj):
     new_column_names = []
     for column_name in list(initial_df.columns.values):
         if type(column_name) is tuple:
@@ -93,7 +236,7 @@ def _rearrange_columns(initial_df):
                 patient_suffix = full_sample_name.split("|")[2]
 
                 new_column_name = "|".join([source,
-                                            _DAMAGING_LABEL,
+                                            obj.column_label,
                                             patient_prefix,
                                             patient_suffix])
                 new_column_names.append(new_column_name)
@@ -101,24 +244,37 @@ def _rearrange_columns(initial_df):
                 new_column_names.append(column_name[0])
 
     initial_df.columns = new_column_names
-    ordered_names = _change_col_order(new_column_names)
+    ordered_names = obj.change_col_order(new_column_names)
 
     initial_df = initial_df[ordered_names]
 
     return initial_df
 
-def _calculate_rank(initial_df):
-    #pylint: disable=line-too-long, unnecessary-lambda
-    initial_df["dbNSFP|overall damaging rank"] = initial_df["dbNSFP_rollup_damaging"].sum(axis=1)
-    initial_df = initial_df.sort("dbNSFP|overall damaging rank", ascending=0)
-    initial_df["dbNSFP|overall damaging rank"] = initial_df["dbNSFP|overall damaging rank"].rank(ascending=0, method="min")
+def rollup_dbnsfp(initial_df):
+    dbnsfp = dbNSFP()
+    melted_df = dbnsfp.melt_df(initial_df)
+    pivoted_df = dbnsfp.pivot_df(melted_df)
+    ranked_df = dbnsfp.calculate_rank(pivoted_df)
+    rearranged_df = _rearrange_columns(ranked_df, dbnsfp)
 
-    try:
-        initial_df["dbNSFP|overall damaging rank"] = initial_df["dbNSFP|overall damaging rank"].apply(lambda x: str(int(x)))
-    except ValueError:
-        pass
+    return rearranged_df
 
-    return initial_df
+def rollup_snpeff(initial_df):
+    snpeff = SnpEff()
+    melted_df = snpeff.melt_df(initial_df)
+    pivoted_df = snpeff.pivot_df(melted_df)
+    ranked_df = snpeff.calculate_rank(pivoted_df)
+    rearranged_df = _rearrange_columns(ranked_df, snpeff)
+
+    return rearranged_df
+
+def combine_dfs(dbnsfp_df, snpeff_df):
+    combined_df = dbnsfp_df.join(snpeff_df, how='outer')
+    combined_df = combined_df.fillna(0)
+
+    combined_df = combined_df.fillna("")
+
+    return combined_df
 
 def _add_arg_parse(args):
     parser = argparse.ArgumentParser()
@@ -131,11 +287,12 @@ def _add_arg_parse(args):
 def rollup(input_file, output_file):
     initial_df = _create_df(input_file)
     condensed_df = _remove_unnecessary_columns(initial_df)
-    melted_df = _melt_df(condensed_df)
-    pivoted_df = _pivot_df(melted_df)
-    ranked_df = _calculate_rank(pivoted_df)
-    rearranged_df = _rearrange_columns(ranked_df)
-    rearranged_df.to_csv(output_file, sep="\t", index=True)
+
+    dbnsfp_df = rollup_dbnsfp(condensed_df)
+    snpeff_df = rollup_snpeff(condensed_df)
+
+    combined_df = combine_dfs(dbnsfp_df, snpeff_df)
+    combined_df.to_csv(output_file, sep="\t", index=True)
 
 def main():
     args = _add_arg_parse(sys.argv[1:])
--- test/rollup_genes_test.py
+++ test/rollup_genes_test.py
@@ -18,10 +18,10 @@ def dataframe(input_data, sep="|"):
 class GeneRollupTestCase(unittest.TestCase):
     def test_create_df(self):
         input_string =\
-'''GENE_SYMBOL\tdbNSFP_rollup_damaging\tJQ_CONS_SOM
+'''GENE_SYMBOL\tdbNSFP_rollup_damaging\tSNPEFF_TOP_EFFECT_IMPACT\tJQ_CONS_SOM
 1\t2\t3'''
         actual_df = rollup_genes._create_df(StringIO(input_string))
-        self.assertEquals(["GENE_SYMBOL", "dbNSFP_rollup_damaging", "JQ_CONS_SOM"],
+        self.assertEquals(["GENE_SYMBOL", "dbNSFP_rollup_damaging", "SNPEFF_TOP_EFFECT_IMPACT", "JQ_CONS_SOM"],
                             list(actual_df.columns.values))
 
     def test_create_df_invalid(self):
@@ -53,31 +53,33 @@ class GeneRollupTestCase(unittest.TestCase):
         self.assertEquals(["GENE_SYMBOL", "dbNSFP_rollup_damaging", "JQ_CONS_SOM_A", "JQ_CONS_SOM_B"],
                           list(actual_df.columns.values))
 
+
+class dbNSFPTestCase(unittest.TestCase):
     def test_melt_df(self):
         input_string =\
-'''GENE_SYMBOL|dbNSFP_rollup_damaging|JQ_CONS_SOM_A|JQ_CONS_SOM_B
-BRCA1|4|2|3
-BRCA1|5|3|4'''
+'''GENE_SYMBOL|dbNSFP_rollup_damaging|SNPEFF_TOP_EFFECT_IMPACT|JQ_CONS_SOM_A|JQ_CONS_SOM_B
+BRCA1|4|.|2|3
+BRCA1|5|.|3|4'''
         input_df = dataframe(input_string)
+        dbNSFP = rollup_genes.dbNSFP()
+        actual_df = dbNSFP.melt_df(input_df)
 
-        actual_df = rollup_genes._melt_df(input_df)
-
-        self.assertEquals(["BRCA1", "4", "JQ_CONS_SOM_A", "2"], list(actual_df.values[0]))
-        self.assertEquals(["BRCA1", "5", "JQ_CONS_SOM_A", "3"], list(actual_df.values[1]))
-        self.assertEquals(["BRCA1", "4", "JQ_CONS_SOM_B", "3"], list(actual_df.values[2]))
-        self.assertEquals(["BRCA1", "5", "JQ_CONS_SOM_B", "4"], list(actual_df.values[3]))
+        self.assertEquals(["BRCA1", ".", "4", "JQ_CONS_SOM_A", "2"], list(actual_df.values[0]))
+        self.assertEquals(["BRCA1", ".", "5", "JQ_CONS_SOM_A", "3"], list(actual_df.values[1]))
+        self.assertEquals(["BRCA1", ".", "4", "JQ_CONS_SOM_B", "3"], list(actual_df.values[2]))
+        self.assertEquals(["BRCA1", ".", "5", "JQ_CONS_SOM_B", "4"], list(actual_df.values[3]))
 
     def test_melt_df_excludeNullGeneSymbols(self):
         input_string =\
-'''GENE_SYMBOL|dbNSFP_rollup_damaging|JQ_CONS_SOM_A|JQ_CONS_SOM_B
-.|4|2|3
-BRCA1|5|3|4'''
+'''GENE_SYMBOL|dbNSFP_rollup_damaging|SNPEFF_TOP_EFFECT_IMPACT|JQ_CONS_SOM_A|JQ_CONS_SOM_B
+.|4|.|2|3
+BRCA1|5|.|3|4'''
         input_df = dataframe(input_string)
+        dbNSFP = rollup_genes.dbNSFP()
+        actual_df = dbNSFP.melt_df(input_df)
 
-        actual_df = rollup_genes._melt_df(input_df)
-
-        self.assertEquals(["BRCA1", "5", "JQ_CONS_SOM_A", "3"], list(actual_df.values[0]))
-        self.assertEquals(["BRCA1", "5", "JQ_CONS_SOM_B", "4"], list(actual_df.values[1]))
+        self.assertEquals(["BRCA1", ".", "5", "JQ_CONS_SOM_A", "3"], list(actual_df.values[0]))
+        self.assertEquals(["BRCA1", ".", "5", "JQ_CONS_SOM_B", "4"], list(actual_df.values[1]))
 
     def test_pivot_df(self):
         input_string =\
@@ -85,8 +87,8 @@ BRCA1|5|3|4'''
 BRCA1|3|JQ_CONS_SOM_A|2
 BRCA1|4|JQ_CONS_SOM_B|3'''
         input_df = dataframe(input_string)
-
-        actual_df = rollup_genes._pivot_df(input_df)
+        dbNSFP = rollup_genes.dbNSFP()
+        actual_df = dbNSFP.pivot_df(input_df)
 
         self.assertEquals([("dbNSFP_rollup_damaging", "JQ_CONS_SOM_A"), ("dbNSFP_rollup_damaging", "JQ_CONS_SOM_B")],
                               list(actual_df.columns.values))
@@ -98,26 +100,12 @@ BRCA1|4|JQ_CONS_SOM_B|3'''
 BRCA1|0|JQ_CONS_SOM_A|2
 BRCA1|4|JQ_CONS_SOM_B|3'''
         input_df = dataframe(input_string)
-
-        actual_df = rollup_genes._pivot_df(input_df)
+        dbNSFP = rollup_genes.dbNSFP()
+        actual_df = dbNSFP.pivot_df(input_df)
 
         self.assertEquals([("dbNSFP_rollup_damaging", "JQ_CONS_SOM_A"), ("dbNSFP_rollup_damaging", "JQ_CONS_SOM_B")],
                               list(actual_df.columns.values))
         self.assertEquals(["0", "4"], list(actual_df.values[0]))
-
-#TODO: determine how to alter dataframe() to account for pivoted df
-    def test_rename_columns(self):
-        input_string =\
-'''GENE_SYMBOL\tdbNSFP_rollup_damaging\tSample\tSample_Data
-BRCA1\t3\tJQ_CONS_SOM|P1|NORMAL\t2
-BRCA1\t4\tJQ_CONS_SOM|P1|TUMOR\t3'''
-        input_df = dataframe(input_string, sep="\t")
-        pivoted_df = rollup_genes._pivot_df(input_df)
-
-        rearranged_df = rollup_genes._rearrange_columns(pivoted_df)
-
-        self.assertEquals("dbNSFP|damaging votes|P1|NORMAL", rearranged_df.columns[0])
-        self.assertEquals("dbNSFP|damaging votes|P1|TUMOR", rearranged_df.columns[1])
 
 #TODO: determine how to alter dataframe() to account for pivoted df
     def test_calculate_rank(self):
@@ -127,9 +115,10 @@ BRCA1\t3\tJQ_CONS_SOM|P1|NORMAL\t2
 BRCA1\t3\tJQ_CONS_SOM|P1|TUMOR\t3
 CREBBP\t7\tJQ_CONS_SOM|P1|NORMAL\t2'''
         input_df = dataframe(input_string, sep="\t")
-        pivoted_df = rollup_genes._pivot_df(input_df)
+        dbNSFP = rollup_genes.dbNSFP()
+        pivoted_df = dbNSFP.pivot_df(input_df)
 
-        ranked_df = rollup_genes._calculate_rank(pivoted_df)
+        ranked_df = dbNSFP.calculate_rank(pivoted_df)
 
         self.assertEquals("BRCA1", ranked_df.ix[1,:].name)
         self.assertEquals("2", ranked_df.ix[1,'dbNSFP|overall damaging rank'].values[0])
@@ -142,14 +131,134 @@ CREBBP\t7\tJQ_CONS_SOM|P1|NORMAL\t2'''
 CREBBP\t7\tJQ_CONS_SOM|P1|NORMAL\t2
 BRCA1\t7\tJQ_CONS_SOM|P1|NORMAL\t2'''
         input_df = dataframe(input_string, sep="\t")
-        pivoted_df = rollup_genes._pivot_df(input_df)
+        dbNSFP = rollup_genes.dbNSFP()
+        pivoted_df = dbNSFP.pivot_df(input_df)
 
-        ranked_df = rollup_genes._calculate_rank(pivoted_df)
+        ranked_df = dbNSFP.calculate_rank(pivoted_df)
 
         self.assertEquals("BRCA1", ranked_df.ix[0,:].name)
         self.assertEquals("1", ranked_df.ix[0,'dbNSFP|overall damaging rank'].values[0])
         self.assertEquals("CREBBP", ranked_df.ix[1,:].name)
         self.assertEquals("1", ranked_df.ix[1,'dbNSFP|overall damaging rank'].values[0])
+
+#TODO: determine how to alter dataframe() to account for pivoted df
+    def test_rename_columns(self):
+        input_string =\
+'''GENE_SYMBOL\tdbNSFP_rollup_damaging\tSample\tSample_Data
+BRCA1\t3\tJQ_CONS_SOM|P1|NORMAL\t2
+BRCA1\t4\tJQ_CONS_SOM|P1|TUMOR\t3'''
+        input_df = dataframe(input_string, sep="\t")
+        dbNSFP = rollup_genes.dbNSFP()
+        pivoted_df = dbNSFP.pivot_df(input_df)
+
+        rearranged_df = rollup_genes._rearrange_columns(pivoted_df, dbNSFP)
+
+        self.assertEquals("dbNSFP|damaging votes|P1|NORMAL", rearranged_df.columns[0])
+        self.assertEquals("dbNSFP|damaging votes|P1|TUMOR", rearranged_df.columns[1])
+
+
+class SnpEffTestCase(unittest.TestCase):
+    def test_melt_df(self):
+        input_string =\
+'''GENE_SYMBOL|dbNSFP_rollup_damaging|SNPEFF_TOP_EFFECT_IMPACT|JQ_CONS_SOM_A|JQ_CONS_SOM_B
+BRCA1|.|HIGH|2|3
+BRCA1|.|LOW|3|4'''
+        input_df = dataframe(input_string)
+        SnpEff = rollup_genes.SnpEff()
+        actual_df = SnpEff.melt_df(input_df)
+
+        self.assertEquals(["BRCA1", "HIGH", ".", "JQ_CONS_SOM_A", "2"], list(actual_df.values[0]))
+        self.assertEquals(["BRCA1", "LOW", ".", "JQ_CONS_SOM_A", "3"], list(actual_df.values[1]))
+        self.assertEquals(["BRCA1", "HIGH", ".", "JQ_CONS_SOM_B", "3"], list(actual_df.values[2]))
+        self.assertEquals(["BRCA1", "LOW", ".", "JQ_CONS_SOM_B", "4"], list(actual_df.values[3]))
+
+    def test_melt_df_excludeNullGeneSymbols(self):
+        input_string =\
+'''GENE_SYMBOL|dbNSFP_rollup_damaging|SNPEFF_TOP_EFFECT_IMPACT|JQ_CONS_SOM_A|JQ_CONS_SOM_B
+.|.|LOW|2|3
+BRCA1|.|HIGH|3|4'''
+        input_df = dataframe(input_string)
+        SnpEff = rollup_genes.SnpEff()
+        actual_df = SnpEff.melt_df(input_df)
+
+        self.assertEquals(["BRCA1", "HIGH", ".", "JQ_CONS_SOM_A", "3"], list(actual_df.values[0]))
+        self.assertEquals(["BRCA1", "HIGH", ".", "JQ_CONS_SOM_B", "4"], list(actual_df.values[1]))
+
+    def test_pivot_df(self):
+        input_string =\
+'''GENE_SYMBOL|dbNSFP_rollup_damaging|SNPEFF_TOP_EFFECT_IMPACT|Sample|Sample_Data
+BRCA1|.|MODERATE|JQ_CONS_SOM_A|2
+BRCA1|.|MODIFIER|JQ_CONS_SOM_B|3'''
+        input_df = dataframe(input_string)
+        SnpEff = rollup_genes.SnpEff()
+        actual_df = SnpEff.pivot_df(input_df)
+
+        self.assertEquals(["MODERATE", "MODIFIER"],
+                              list(actual_df.columns.values))
+        self.assertEquals([1, 0], list(actual_df.values[0]))
+
+    def test_pivot_df_ifNull(self):
+        input_string =\
+'''GENE_SYMBOL|dbNSFP_rollup_damaging|SNPEFF_TOP_EFFECT_IMPACT|Sample|Sample_Data
+BRCA1|.|.|JQ_CONS_SOM_A|2
+BRCA1|.|HIGH|JQ_CONS_SOM_B|3'''
+        input_df = dataframe(input_string)
+        SnpEff = rollup_genes.SnpEff()
+        actual_df = SnpEff.pivot_df(input_df)
+
+        self.assertEquals([".","HIGH"],
+                              list(actual_df.columns.values))
+        self.assertEquals([1, 0], list(actual_df.values[0]))
+
+    def test_calculate_rank(self):
+        input_string =\
+'''GENE_SYMBOL\tSNPEFF_TOP_EFFECT_IMPACT\tSample\tSample_Data
+BRCA1\tHIGH\tJQ_CONS_SOM|P1|NORMAL\t2
+BRCA1\tLOW\tJQ_CONS_SOM|P1|TUMOR\t3
+CREBBP\tMODERATE\tJQ_CONS_SOM|P1|NORMAL\t2'''
+        input_df = dataframe(input_string, sep="\t")
+        SnpEff = rollup_genes.SnpEff()
+        pivoted_df = SnpEff.pivot_df(input_df)
+
+        ranked_df = SnpEff.calculate_rank(pivoted_df)
+
+        self.assertEquals("BRCA1", ranked_df.ix[0,:].name)
+        self.assertEquals("1", ranked_df.ix[0,'SnpEff|overall impact rank'].values[0])
+        self.assertEquals("CREBBP", ranked_df.ix[1,:].name)
+        self.assertEquals("2", ranked_df.ix[1,'SnpEff|overall impact rank'].values[0])
+
+    def test_calculate_rank_tie(self):
+        input_string =\
+'''GENE_SYMBOL\tSNPEFF_TOP_EFFECT_IMPACT\tSample\tSample_Data
+CREBBP\tLOW\tJQ_CONS_SOM|P1|NORMAL\t2
+BRCA1\tLOW\tJQ_CONS_SOM|P1|NORMAL\t2'''
+        input_df = dataframe(input_string, sep="\t")
+        SnpEff = rollup_genes.SnpEff()
+        pivoted_df = SnpEff.pivot_df(input_df)
+
+        ranked_df = SnpEff.calculate_rank(pivoted_df)
+
+        self.assertEquals("BRCA1", ranked_df.ix[0,:].name)
+        self.assertEquals("1", ranked_df.ix[0,'SnpEff|overall impact rank'].values[0])
+        self.assertEquals("CREBBP", ranked_df.ix[1,:].name)
+        self.assertEquals("1", ranked_df.ix[1,'SnpEff|overall impact rank'].values[0])
+
+    def test_rename_columns(self):
+        input_string =\
+'''GENE_SYMBOL\tdbNSFP_rollup_damaging\tSNPEFF_TOP_EFFECT_IMPACT\tJQ_CONS_SOM|P1|NORMAL\tJQ_CONS_SOM|P1|TUMOR
+BRCA1\t.\tHIGH\t2\t3
+BRCA1\t.\tLOW\t3\t4'''
+        input_df = dataframe(input_string, sep="\t")
+        SnpEff = rollup_genes.SnpEff()
+        melted_df = SnpEff.melt_df(input_df)
+        pivoted_df = SnpEff.pivot_df(melted_df)
+        ranked_df = SnpEff.calculate_rank(pivoted_df)
+
+        rearranged_df = rollup_genes._rearrange_columns(ranked_df, SnpEff)
+
+        self.assertEquals("SnpEff|impact|P1|NORMAL", rearranged_df.columns[1])
+        self.assertEquals("SnpEff|impact|P1|TUMOR", rearranged_df.columns[2])
+
 
 class GeneRollupFunctionalTestCase(unittest.TestCase):
     def test_rollup_genes(self):
